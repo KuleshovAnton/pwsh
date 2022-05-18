2ca9cc03ef056501068dafdbef3a0ffe9e9803d9
KuleshovAnton <74101963+KuleshovAnton@users.noreply.github.com>
2022-05-18 12:52:49 +0300

    Add files via upload

---
+++ manageGrafanaWithAPI.psm1
@@ -0,0 +1,160 @@
+﻿function Convert-GrafCredential{
+    <#
+    .SYNOPSIS
+        Function for converting plain text authentication informations to base 64
+    .EXAMPLE
+        Convert-GrafCredential -login foo -password bar
+    #>
+    param(
+        [Parameter(Mandatory=$true)][string]$Login,
+        [Parameter(Mandatory=$true)][string]$Password
+        )
+    return [Convert]::ToBase64String( [Text.Encoding]::ASCII.GetBytes(( "{0}:{1}" -f $login,$password) ) )
+}
+
+function Create-GrafDatasourceParameters {
+    param(
+        [parameter(Mandatory=$true,position=0)]$Method,
+        [parameter(Mandatory=$true,position=1)]$URI,
+        [parameter(Mandatory=$true,position=2)]$Credential
+        )
+    $datasourceParameters = @{
+        Method = $Method
+        URI = $URI
+        Headers = @{"Authorization" = "Basic $Credential"}
+        ContentType = "application/json;charset=UTF-8"
+        }
+    return $datasourceParameters
+}
+
+###TEAMS
+#Get all Teams.
+function Get-GrafTeams {
+    <#
+    .SYNOPSIS
+        Grafana Function Get all Teams. 
+    .EXAMPLE
+        Get-GrafTeams -Login foo -Password foo -Url "http://_IP_or_DNS_:Port"
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url
+        )
+    $credential = Convert-GrafCredential -Login $Login -Password $Password
+    $createDatasourceUri = "$Url/api/teams/search?"
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "Get" -URI $createDatasourceUri -Credential $credential
+    return $result = (Invoke-RestMethod @datasourceParameters).teams
+}
+
+##Get Team members.
+function Get-GrafTeamsMembers {
+    <#
+    .SYNOPSIS
+        Grafana Function Get Teams members. 
+    .EXAMPLE
+        Get-GrafTeamsMember -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -teamId number_TeamId
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url,
+        [parameter(Mandatory=$true,position=3)]$teamId 
+        )
+    $credential = Convert-GrafCredential -Login $Login -Password $Password
+    $createDatasourceUri = "$Url/api/teams/$teamId/members"
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "Get" -URI $createDatasourceUri -Credential $credential
+    return $result = Invoke-RestMethod @datasourceParameters
+}
+
+#Add a user to the Team.
+function Add-GrafTeamsMembers {
+    <#
+    .SYNOPSIS
+        Grafana Function Add a user to the Team.
+    .EXAMPLE
+        Add-GrafTeamsMembers -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -teamId number_TeamId -userId number_UserId
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url,
+        [parameter(Mandatory=$true,position=3)]$teamId,
+        [parameter(Mandatory=$true,position=4)]$userId
+        )
+    $credential = Convert-GrafCredential -Login $Login -Password $Password
+    $createDatasourceUri = "$Url/api/teams/$teamId/members"
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "POST" -URI $createDatasourceUri -Credential $credential
+    $body = ("{"+"""userId"""+":"+ $userId +"}")
+    $result = (Invoke-RestMethod @datasourceParameters -Body $body).message 
+    return $arr = @(New-Object PSObject -Property @{"userId"=$userId; "message"=$result; "teamId"=$teamId})
+}
+
+###USER
+#Get all users.
+function Get-GrafUsers {
+    <#
+    .SYNOPSIS
+        Grafana Function Get all users
+    .EXAMPLE
+        Get-GrafUsers -Login foo -Password foo -Url "http://_IP_or_DNS_:Port"
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url
+        )
+    $credential = Convert-GrafCredential -Login $Login -Password $Password
+    $createDatasourceUri = "$Url/api/users"
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "Get" -URI $createDatasourceUri -Credential $credential
+    return $result = Invoke-RestMethod @datasourceParameters 
+}
+
+#Get user using Id or login/email.
+function Get-GrafUser {
+    <#
+    .SYNOPSIS
+        Grafana Function Get user using Id or login/email.
+    .EXAMPLE
+        Get-GrafUser -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -$userId number_UserId
+        Get-GrafUser -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -$userLoginOrEmail User1
+        Get-GrafUser -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -$userLoginOrEmail User1@email.foo
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url,
+        [parameter(Mandatory=$false,position=3)]$userId,
+        [parameter(Mandatory=$false,position=4)]$userLoginOrEmail
+        )
+    $credential = Convert-GrafCredential -Login $Login -Password $Password
+    if ( $userId ) {
+        $createDatasourceUri = "$Url/api/users/$userId"
+        }
+    else {
+        $createDatasourceUri = "$Url/api/users/lookup?loginOrEmail=$userLoginOrEmail"
+        }
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "Get" -URI $createDatasourceUri -Credential $credential
+    return $result = Invoke-RestMethod @datasourceParameters 
+}
+
+#Get which groups the user is in.
+function Get-GrafUserTeams {
+    <#
+    .SYNOPSIS
+        Grafana Function Get which groups the user is in.
+    .EXAMPLE
+        Get-GrafUser -Login foo -Password foo -Url "http://_IP_or_DNS_:Port" -$userId number_UserId
+    #>
+    param(
+        [parameter(Mandatory=$true,position=0)]$Login,
+        [parameter(Mandatory=$true,position=1)]$Password,
+        [parameter(Mandatory=$true,position=2)]$Url,
+        [parameter(Mandatory=$true,position=3)]$userId
+        )
+    $createDatasourceUri = "$Url/api/users/$userId/teams"
+    $datasourceParameters = Create-GrafDatasourceParameters -Method "Get" -URI $createDatasourceUri -Credential $credential
+    return $result = Invoke-RestMethod @datasourceParameters 
+}
+
+Export-ModuleMember -Function Get-GrafTeams,Get-GrafTeamsMembers,Add-GrafTeamsMembers,Get-GrafUsers,Get-GrafUser,Get-GrafUserTeams
