712b2c94e5828539fd8493ab78d0d9ba26b4a03b
KuleshovAnton <74101963+KuleshovAnton@users.noreply.github.com>
2025-11-27 09:04:17 +0300

    Update alertmanager_webhook_services_async.ps1

--- Prometheus/AlertManager/alertmanager_webhook_services_async.ps1
+++ Prometheus/AlertManager/alertmanager_webhook_services_async.ps1
@@ -3,10 +3,15 @@
 #v_1.0.0.3
 #Accept and send a alertmanager_webhook.
 #Shutdown listener port. Example: #curl http://localhost:8080/api/end
+
 #Create listener port.
 $listener = New-Object System.Net.HttpListener
 $listener.Prefixes.Add('http://localhost:8080/api/')
 $listener.Start()
+
+#$userAuth = ''
+#$userAuthPass = ''
+#$urlVCS = ''
 
 while($true){
     $context = $listener.GetContextAsync()
